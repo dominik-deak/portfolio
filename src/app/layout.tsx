859e4f280d4b302154be6e148bd2f701fe7987f8
dominik <105545749+dominik-deak@users.noreply.github.com>
2025-10-29 14:13:25 +0000

    Add @vercel/speed-insights dependency and integrate SpeedInsights component in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+
 import "./globals.css";
 
 const geistSans = Geist({
@@ -29,6 +31,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
