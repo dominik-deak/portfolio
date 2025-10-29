300e7be551cb043771db5ddf85a9d3227785c562
dominik <105545749+dominik-deak@users.noreply.github.com>
2025-10-29 16:06:54 +0000

    Revert "Add @vercel/speed-insights dependency and integrate SpeedInsights component in layout"
    This reverts commit 859e4f280d4b302154be6e148bd2f701fe7987f8.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,5 @@
-import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-
 import "./globals.css";
 
 const geistSans = Geist({
@@ -31,7 +29,6 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-        <SpeedInsights />
       </body>
     </html>
   );
