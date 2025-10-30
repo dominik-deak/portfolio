55f6cbe10a4415c11b65d0612ea2bcf9a2a9c344
dominik <105545749+dominik-deak@users.noreply.github.com>
2025-10-30 13:06:19 +0000

    Swap project details in JSON and reverse project display order on the Projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -18,7 +18,7 @@ const ProjectsPage = () => {
               engineering.
             </p>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-              {projectsData.map((project) => (
+              {projectsData.reverse().map((project) => (
                 <ProjectCard key={project.id} project={project} />
               ))}
             </div>
